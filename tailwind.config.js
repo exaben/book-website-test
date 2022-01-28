c5efcf6e13fb20665974f609272e9a1f5b3627a3
Gal Bence <gallaci5@gmail.com>
2022-01-28 16:39:29 +0100

    test search

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        colors: {
+          'menubar': 'rgba(255,248,220,1)',
+          'background': 'rgba(222,184,135,1)',
+          'button': 'rgba(255,222,173,1)',
+        },
+    },
+  },
+  plugins: [],
+}
